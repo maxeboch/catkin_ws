7ce8a632e6b9c3cda71d22b205ea9f2d93c2f11c
Max Eboch <max.eboch@magna.com>
2018-06-06 14:40:09 -0400

    example 2 complete

---
+++ build/chapter2_tutorials/CMakeFiles/example2_a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example2_a.dir/src/example2_a.cpp.o"
+  "/home/max/catkin_ws/devel/lib/chapter2_tutorials/example2_a.pdb"
+  "/home/max/catkin_ws/devel/lib/chapter2_tutorials/example2_a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example2_a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
